68a4cff506d01b4e663fece241ef4faf8b75af0f
Jaynicka Arrieta <kiira@fb.com>
2018-07-10 16:58:27 -0700

    able to grab username and post from parse

--- app/src/main/java/ilovecats/com/parsetagram/ParseApp.java
+++ app/src/main/java/ilovecats/com/parsetagram/ParseApp.java
@@ -3,12 +3,16 @@ package ilovecats.com.parsetagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
+
+import ilovecats.com.parsetagram.model.Post;
 
 public class ParseApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
 
+        ParseObject.registerSubclass(Post.class);
         //sets up parse server
         final Parse.Configuration config = new Parse.Configuration.Builder(getApplicationContext())
                 .applicationId("jaynicka-parse")
